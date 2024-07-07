ce105daa931b75ab38b27e98198fcb1d9756065e
Devanand kr Rana <gdsc.devanandrana@gmail.com>
2024-07-07 15:02:52 +0530

    export the .env and Start the Server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const express = require("express");
+const { PORT } = require("./src/config/serverConfig");
+
+const app = express();
+
+const setupAndStartService = () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+  });
+};
+
+setupAndStartService();
