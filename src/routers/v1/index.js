85bbddbf8ebe3a917f3118ec5c5d5a7f499f7ce7
Devanand kr Rana <gdsc.devanandrana@gmail.com>
2024-07-08 18:07:32 +0530

    Added the Routers

---
+++ src/routers/v1/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const controller = require("../../controllers/student-controller");
+
+const router = express.Router();
+
+router.post("/stu", controller.create);
+router.delete("stu/:id", controller.destroy);
+
+module.exports = router;
